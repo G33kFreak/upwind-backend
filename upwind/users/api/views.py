d18e17c70010e20ad3e067ffb4606a041cf75975
Ruslan Zaripov <zaripo.rus@gmail.com>
2022-03-26 21:29:14 +0100

    Remove unused endpoint

--- upwind/users/api/views.py
+++ upwind/users/api/views.py
@@ -5,8 +5,6 @@ from rest_framework.exceptions import AuthenticationFailed
 from users.api.serializers import UserSerializer
 from users.models import User
 
-import jwt, datetime
-
 class RegisterAPIView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
@@ -14,22 +12,4 @@ class RegisterAPIView(APIView):
         user = serializer.create()
         user.save()
         return Response(serializer.data)
-
-class LoginAPIView(APIView):
-    def post(self, request):
-        email = request.data['email']
-        password = request.data['password']
-
-        user = User.objects.filter(email=email).first()
-
-        if user is None:
-            raise AuthenticationFailed('User not found')
-
-        if not user.check_password(password):
-            raise AuthenticationFailed('Incorrect password')
-        
-        return Response({
-            "message":"Success"
-        })
-
         
--- upwind/users/urls.py
+++ upwind/users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from users.api.views import RegisterAPIView, LoginAPIView
+from users.api.views import RegisterAPIView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view()),
-    path('login/', LoginAPIView.as_view()),
 ]
