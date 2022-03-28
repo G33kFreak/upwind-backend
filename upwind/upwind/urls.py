570fdd59a5838a3b12abdf3d25b6da329eeddf6b
Ruslan Zaripov <zaripo.rus@gmail.com>
2022-03-28 23:09:20 +0200

    add gitignore

--- upwind/upwind/urls.py
+++ upwind/upwind/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+api_urls = [
+    path('', include('users.urls')),
+    path('', include('habits.urls'))
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
-    path('api/', include('jwt_auth.urls')),
-    path('api/', include('habits.urls'))
+    path('api/', include(api_urls)),
+    path('', include('jwt_auth.urls'))
 ]
