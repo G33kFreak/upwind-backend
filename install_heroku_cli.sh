cb5318cf7549f2814441306d0ad67fe9f77abf70
Ruslan Zaripov <zaripo.rus@gmail.com>
2022-04-11 11:10:06 +0200

    WIP: add CD

--- install_heroku_cli.sh
+++ install_heroku_cli.sh
@@ -1,8 +1,4 @@
-wget -q https://cli-assets.heroku.com/heroku-linux-x64.tar.gz -O heroku.tar.gz
-tar -xzf heroku.tar.gz
-mkdir -p ~/bin/heroku-cli
-tar -xzf heroku.tar.gz --strip-components 1 -C ~/bin/heroku-cli
-rm -fr heroku.tar.gz
+wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
 ~/bin/heroku-cli/bin/heroku -v
 cat >~/.netrc <<EOF
 machine api.heroku.com
